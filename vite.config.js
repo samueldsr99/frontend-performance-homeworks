8ce614570b657053757a09022d8bb950ebfa8e5a
Samuel David Suárez Rodríguez <samueldsr8@gmail.com>
2024-03-19 11:16:35 +0100

    chore: add multiple input entrypoints

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,19 @@
 import { defineConfig } from "vite";
 import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [ViteImageOptimizer({})],
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, "index.html"),
+        cart: resolve(__dirname, "cart.html"),
+        checkout: resolve(__dirname, "checkout.html"),
+        contact: resolve(__dirname, "contact.html"),
+        detail: resolve(__dirname, "detail.html"),
+        shop: resolve(__dirname, "shop.html"),
+      },
+    },
+  },
 });
