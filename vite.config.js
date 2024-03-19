da2d92754e9fbb96dab2804a3f54159d6eaa3fd8
Samuel David Suárez Rodríguez <samueldsr8@gmail.com>
2024-03-19 12:13:22 +0100

    refactor: reorder files to minify assets

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,11 @@ import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 import { resolve } from "path";
 
 export default defineConfig({
+  base: "/",
   plugins: [ViteImageOptimizer({})],
+  publicDir: "public",
   build: {
+    outDir: "dist",
     rollupOptions: {
       input: {
         index: resolve(__dirname, "index.html"),
@@ -13,6 +16,12 @@ export default defineConfig({
         contact: resolve(__dirname, "contact.html"),
         detail: resolve(__dirname, "detail.html"),
         shop: resolve(__dirname, "shop.html"),
+      },
+
+      output: {
+        entryFileNames: "[name]-[hash].js",
+        chunkFileNames: "chunks/[name]-[hash].js",
+        assetFileNames: "assets/[name]-[hash].[ext]",
       },
     },
   },
