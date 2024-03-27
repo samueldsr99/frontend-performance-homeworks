7141b7a702c8fddf416fdbd8d3f8d996cb8e2e10
Samuel David Suárez Rodríguez <samueldsr8@gmail.com>
2024-03-27 11:33:43 +0100

    feat(FoodCarousel): add

---
+++ src/app/components/FoodCarousel.tsx
@@ -0,0 +1,100 @@
+import Image from "next/image";
+
+export interface CarouselItem {
+  title: string;
+  active?: boolean;
+  image: {
+    src: string;
+    alt: string;
+    width: number;
+    height: number;
+    priority?: boolean;
+  };
+  caption: string;
+  price: number;
+  rating: number;
+}
+
+export interface FoodCarouselProps {
+  items: CarouselItem[];
+}
+
+const StarRating = ({ rating }: { rating: number }) => {
+  const integerRating = Math.floor(rating);
+
+  return Array.from({ length: 5 }, (_, index) => {
+    return (
+      <div className="reviews-stars">
+        <i
+          key={index}
+          className={`bi-star${
+            index < integerRating ? "-fill" : ""
+          } reviews-icon`}
+        ></i>
+      </div>
+    );
+  });
+};
+
+export default function FoodCarousel({ items }: FoodCarouselProps) {
+  return (
+    <div
+      id="carouselExampleCaptions"
+      className="carousel carousel-fade hero-carousel slide"
+      data-bs-ride="carousel"
+    >
+      <div className="carousel-inner">
+        {items.map((item) => (
+          <div className={`carousel-item ${item.active ? "active" : ""}`}>
+            <div className="carousel-image-wrap">
+              <Image
+                priority={item.image.priority}
+                src={item.image.src}
+                width={800}
+                height={600}
+                className="img-fluid carousel-image"
+                alt={item.image.alt}
+              />
+            </div>
+
+            <div className="carousel-caption">
+              <div className="d-flex align-items-center">
+                <h4 className="hero-text">{item.title}</h4>
+
+                <span className="price-tag ms-4">
+                  <small>$</small>
+                  {item.price}
+                </span>
+              </div>
+
+              <div className="d-flex flex-wrap align-items-center">
+                <h5 className="reviews-text mb-0 me-3">{item.rating}/5</h5>
+                <StarRating rating={item.rating} />
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <button
+        className="carousel-control-prev"
+        type="button"
+        data-bs-target="#carouselExampleCaptions"
+        data-bs-slide="prev"
+      >
+        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Previous</span>
+      </button>
+
+      <button
+        className="carousel-control-next"
+        type="button"
+        data-bs-target="#carouselExampleCaptions"
+        data-bs-slide="next"
+      >
+        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Next</span>
+      </button>
+    </div>
+  );
+}
