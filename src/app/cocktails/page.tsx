075bfe68a67d4c8cb2fa02ea7c2414fdcea64b79
Samuel David Suárez Rodríguez <samueldsr8@gmail.com>
2024-03-28 14:33:38 +0100

    bugfix: fix build issues

--- src/app/cocktails/page.tsx
+++ src/app/cocktails/page.tsx
@@ -1,27 +1,29 @@
-"use client";
-
 import { Suspense } from "react";
 import CocktailsList from "./components/CocktailsList";
 
 import styles from "./styles.module.css";
 import Skeleton from "./components/Skeleton/Skeleton";
 import SearchForm from "./components/SearchForm";
-import { useSearchParams } from "next/navigation";
+
+export const dynamic = "force-dynamic";
 
 function LoadingState() {
   return Array.from({ length: 10 }).map((_, index) => <Skeleton key={index} />);
 }
 
-export default function CocktailsPage() {
-  const searchParams = useSearchParams();
-  const search = searchParams.get("s");
+export default function CocktailsPage({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | undefined };
+}) {
+  const search = searchParams.s;
 
   return (
     <div className={styles.root}>
       <h1 className={styles.pageTitle}>Cocktails</h1>
       <SearchForm />
       <div className={styles.pageContent}>
-        <Suspense key={search} fallback={<LoadingState />}>
+        <Suspense key={`${search}`} fallback={<LoadingState />}>
           <CocktailsList term={search ?? "margarita"} />
         </Suspense>
       </div>
