8bfbe72029979edf824693697ac2eaa9e953033e
Samuel David Suárez Rodríguez <samueldsr8@gmail.com>
2024-03-28 15:09:55 +0100

    feat: integrate sentry & 404 & 500 routes

---
+++ src/app/error.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function GlobalError() {
+  return (
+    <div>
+      <h1 className="text-center">Oops</h1>
+      <p className="text-center">Something went wrong on our side</p>
+    </div>
+  );
+}
---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+export default function NotFound() {
+  return (
+    <div>
+      <h1 className="text-center">Not Found</h1>
+      <p className="text-center">
+        Sorry, the page you are looking for does not exist.
+      </p>
+    </div>
+  );
+}
